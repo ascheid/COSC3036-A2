ca25b2981f1398b1af6c8a5e75c135ff0358ace7
Alvaro Scheid <ascheid@gmail.com>
2017-10-21 00:11:50 -0300

    First Commit

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+
+    public float moveSpeed;
+    private Animator animator;
+    private bool moving;
+    private Vector2 lastMove;
+
+	// Use this for initialization
+	void Start () {
+        animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        moving = false;
+
+        //Right
+        if(Input.GetAxisRaw("Horizontal") > 0.1f)
+        {
+            transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
+            moving = true;
+        }
+
+        //Left
+        if(Input.GetAxisRaw("Horizontal") < -0.1f)
+        {
+            transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
+            moving = true;
+            lastMove = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        }
+
+        //Up
+        if (Input.GetAxisRaw("Vertical") > 0.1f)
+        {
+            transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f));
+            moving = true;
+            lastMove = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        }
+
+        //Down
+        if (Input.GetAxisRaw("Vertical") < -0.1f)
+        {
+            transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f));
+            moving = true;
+            lastMove = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        }
+
+
+        animator.SetFloat("MoveX", Input.GetAxisRaw("Horizontal"));
+        animator.SetFloat("MoveY", Input.GetAxisRaw("Vertical"));
+
+        animator.SetBool("Moving", moving);
+        animator.SetFloat("LastMoveX", lastMove.x);
+        animator.SetFloat("LastMoveY", lastMove.y);
+
+
+	}
+}
